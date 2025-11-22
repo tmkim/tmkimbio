cbef490d20f5e3c7f8b656ec01186f5d68e75863
tmkim <taeminkim13@gmail.com>
2025-11-22 18:46:59 -0500

    update contact section and modal

--- portfolio/src/app/test/sections/contact.tsx
+++ portfolio/src/app/test/sections/contact.tsx
@@ -2,36 +2,48 @@
 
 import { motion } from "framer-motion";
 import { fadeUp } from "@/lib/animations";
+import { useState } from "react";
+import ContactModal from '@/components/contact-modal';
 
 export default function Contact() {
+    const [contactModal, setContactModal] = useState<boolean>(false);
+  
   return (
     <motion.div {...fadeUp} className="text-center space-y-4 p-6">
       <h2 className="text-4xl font-semibold">Let's Talk</h2>
 
-      <p className="text-gray-600">
+      <p>
         I love connecting with people who build cool things.
       </p>
 
-      <div className="flex gap-4 justify-center">
-        <a
-          href="mailto:youremail@example.com"
-          className="text-blue-600 hover:underline"
-        >
-          Email
-        </a>
-        <a
-          href="https://github.com/yourname"
-          className="text-blue-600 hover:underline"
-        >
-          GitHub
-        </a>
-        <a
-          href="https://linkedin.com/in/yourname"
-          className="text-blue-600 hover:underline"
-        >
-          LinkedIn
-        </a>
-      </div>
+        <div className="mt-6 flex gap-4 text-black">
+          <button
+            className="flex items-center justify-center min-w-[160px] px-4 py-2 text-lg font-semibold bg-[var(--accent)] rounded-md hover:cursor-pointer"
+            onClick={() => setContactModal(true)}
+          >
+            <p className="hover:underline">Send me an Email</p>
+          </button>
+          <button
+            className="flex items-center justify-center min-w-[160px] px-4 py-2 text-lg font-semibold bg-[var(--accent-2)] rounded-md hover:cursor-pointer"
+          >
+            <a href="https://www.linkedin.com/in/tmk13" className="hover:underline"
+            target="_blank" rel="noopener noreferrer">Find me on LinkedIn</a>
+          </button>
+          <button
+            className="flex items-center justify-center min-w-[160px] px-4 py-2 text-lg font-semibold bg-[var(--accent-3)] rounded-md hover:cursor-pointer"
+          >
+            <a href="https://www.github.com/tmkim" className="hover:underline"
+            target="_blank" rel="noopener noreferrer">Check out my Github</a>
+          </button>
+        </div>
+
+      {contactModal && (
+        <div>
+          <ContactModal 
+          onClose={() => setContactModal(false)} 
+          />
+        </div>
+      )}
     </motion.div>
   );
 }
--- portfolio/src/components/contact-modal.tsx
+++ portfolio/src/components/contact-modal.tsx
@@ -5,31 +5,31 @@ interface ContactProps {
 }
 const ContactModal: React.FC<ContactProps> = ({onClose}) => {
     return (
-    <div className="fixed inset-0 flex items-center justify-center">
+    <div className="fixed inset-0 flex items-center justify-center text-black">
         {/* Background overlay */}
         <div className="absolute inset-0 bg-gray-500" style={{ backgroundColor: 'rgba(31, 41, 55, 0.5)' }} onClick={onClose} />
       
         {/* Modal content */}
-        <div className="relative bg-white rounded-lg p-4 max-w-lg w-full">
+        <div className="relative bg-gray-100 rounded-lg p-4 max-w-lg w-full">
           <h2 className="text-2xl font-semibold mb-4">Contact Me</h2>
           <form action="https://formspree.io/f/xqapzqwn" method="POST" className="space-y-4">
             <div>
-              <label className="block text-gray-700 font-medium">Your Name</label>
+              <label className="block font-medium">Your Name</label>
               <input type="text" name="name" required className="w-full p-2 border rounded-lg" />
             </div>
             <div>
-              <label className="block text-gray-700 font-medium">Your Email</label>
+              <label className="block font-medium">Your Email</label>
               <input type="email" name="email" required className="w-full p-2 border rounded-lg" />
             </div>
             <div>
-              <label className="block text-gray-700 font-medium">Message</label>
+              <label className="block font-medium">Message</label>
               <textarea name="message" required className="w-full p-2 border rounded-lg"></textarea>
             </div>
             <div className="mt-6 flex justify-end gap-4">
-              <button type="button" onClick={onClose} className="px-6 py-2 bg-gray-300 rounded-md hover:bg-gray-400">
+              <button type="button" onClick={onClose} className="px-6 py-2 rounded-md text-white bg-red-600 hover:bg-red-400 hover:cursor-pointer">
                 Cancel
               </button>
-              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+              <button type="submit" className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-400 hover:cursor-pointer">
                 Send Message
               </button>
             </div>
